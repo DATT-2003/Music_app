b32ef474d2ff66e5c813375b2c09d1b52f35ba86
DATT-2003 <dattien52003@gmail.com>
2023-11-26 17:12:02 +0700

    Project

---
+++ app/src/main/java/com/example/music_app/playMusic.kt
@@ -0,0 +1,11 @@
+package com.example.music_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class playMusic : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_music)
+    }
+}
